b22ed941ad4a9e32170e2ded7301c1416a6d085a
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-10-13 00:26:39 +0600

    Create 04_bigMod.cpp

---
+++ 04_bigMod.cpp
@@ -0,0 +1,11 @@
+// Complexity O(log n)
+
+int bigMod(int num, int power, int mod)
+{
+    if(power==0) return 1;
+    int v=bigMod(num,power/2,mod);
+    v= (v*v)%mod;
+    if(power%2==1) v=(v*num)%mod; // if power is odd
+    return v;
+}
+
