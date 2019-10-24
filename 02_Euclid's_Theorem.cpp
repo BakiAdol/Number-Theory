0b7df00a89c868b5ec21a1cb99e75dae0ede0600
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-10-25 00:51:36 +0600

    Create 02_Euclid's_Theorem.cpp

---
+++ 02_Euclid's_Theorem.cpp
@@ -0,0 +1,7 @@
+Statement : "There are infinite primely many primes"
+Proof by conduction:
+  Let's some list of primes p1,p2,p3,p4....pn
+  Now P = p1*p2*p3*p4*...pn + 1
+  n! is divided by 2 to n
+  Here p1 to pn not divied P, they can divided P-1;
+  So P is another prime that not present int this list p1, p2.... pn
