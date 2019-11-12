e85f09e3b9fd8d712b18c18edb7c4af6b7f773cb
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-11-12 19:18:25 +0600

    Update 04_bigMod.cpp

--- 04_bigMod.cpp
+++ 04_bigMod.cpp
@@ -5,7 +5,7 @@ int bigMod(int num, int power, int mod)
     if(power==0) return 1;
     int v=bigMod(num,power/2,mod);
     v= (v*v)%mod;
-    if(power%2==1) v=(v*num)%mod; // if power is odd
+    if((power&1)==1) v=(v*num)%mod; // if power is odd
     return v;
 }
 
