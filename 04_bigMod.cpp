8d295be0f47e3d3886de38dd157d57b75c553bef
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2021-03-01 07:46:16 +0600

    Update 04_bigMod.cpp

--- 04_bigMod.cpp
+++ 04_bigMod.cpp
@@ -24,25 +24,3 @@ ll bigMod(ll num, ll base, ll mod)
     return res;
 }
 
-
-typedef long long int lli;
-// What is the a*b mod m where m = 10^15
-lli MultiplicationViaAddition(lli a, lli b, lli m)
-{
-	if(b==1) return a;
-	if(b==0) return 0;
-	
-	lli ans = ((MultiplicationViaAddition(a,b/2,m)%m)*2)%m;
-
-	if(b&1) ans = (ans + (a%m))%m;
-	
-	return ans;
-}
-
-//what is a+b mod m where m is limited but a+b coudn't be store in memory
-lli AddAndMod(lli a, lli b, lli m)
-{
-	if(m-a<=b) return (a+b)%m;
-	return b-(m-a);
-}
-
