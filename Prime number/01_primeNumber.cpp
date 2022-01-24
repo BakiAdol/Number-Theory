4397ad26c858b940efba9b80291efa78a8d5aead
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2022-01-24 23:26:29 +0600

    Check and generate prime number

--- Prime number/01_primeNumber.cpp
+++ Prime number/01_primeNumber.cpp
@@ -1,124 +1,88 @@
+#include <bits/stdc++.h>
 
-//---------------------------Test a number is prime or not----------------------------------------
-/*
-* Time Complexity O(squere root of n)
-*/
+using namespace std;
 
-bool primeTest(int n)
+// check a number is prime or not
+// complexity O(sqrt(n))
+bool isPrime(int n)
 {
     if(n<2) return false;
-    int limit = sqrt(n);
-    for(int i=2;i<=limit;i++)
+
+    for(int i=2;i*i<=n;i++)
     {
-        if(n%i==0)
-        {
-            return false;
-        }
+        if(n%i==0) return false;
     }
     return true;
 }
 
-//--------------------------------------------------------------------------------------------------
-
-/*
-* Using Eratosthenes's Sieve
-* Time Complexity O(n log log n)
-* Here the number of iteration O(n) + Summation of n/p; where p is prime number and n>=p
-* From Merten's Second Theorem, Summation of n/p = log log n + O(1)
-*/
-//-----------------------------------Generate Prime Numbers----------------------------------------
-void prime()
+// generate prime numbers
+// complexity O(n log(log(n)))
+vector<int> primes;
+void primeGenerate()
 {
-    int range=100001;
-    int primes[range];
-    primes[0]=0,primes[1]=0;
-    for(int i=2;i<range;i++)
+    bool isPrime[105];
+    for(int i=2;i<101;i++)
     {
-        primes[i]=1;
-    }
-    for(int i=2;i<range;i++)
-    {
-        if(primes[i])
+        if(isPrime[i]==0)
         {
-            for(int j=2;i*j<range;j++)
+            for(int j=2;i*j<101;j++)
             {
-                primes[i*j]=0;
+                isPrime[i*j]=1;
             }
         }
     }
-    for(int i=0;i<range;i++)
-    {
-        if(primes[i])
-        {
-            cout << i << endl;
-        }
-    }
+    for(int i=2;i<101;i++)
+    if(isPrime[i]==0) 
+    primes.push_back(i);
 }
 
-//--------------------------------------------------------------------------------------------------
-
-///------------------------------------------ Optimeze seive ---------------------------------------
-void prime()
+// Optimized prime generation
+// there are only 2 is even prime, so we check for only odd numbers
+void primeGenerateOptimized()
 {
-	/**
-	 * 
-	 *  -> only 2 is even number that is prime, so we check only odd numbers
-	 *  -> Suppose We check for odd=3, so next cut number is odd*2=6, here 6 is even
-	 * 	   so it's completely unnecessary. so we start tmp=odd, odd=tmp*tmp and increase
-	 *     odd+=tmp*2, then we get every time odd number.
-	 * 
-	 * */
-	
-	long long int range=1000001;
-	bool check[range];
-	vector<int> primes;
-	
-	primes.push_back(2);
-	
-	for(long long int i=3;i<range;i+=2)
-	{
-		if(check[i]==0)
-		{
-			primes.push_back(i);
-			
-			for(long long int j=i*i;j<range;j+=i*2) check[j]=1;
-		}
-	}
-	
-	for(int i=0;i<10;i++) cout << primes[i] << endl;
-}
-///-------------------------------------------------------------------------------------------------
-
-//---------------------------------------Prime Factorization----------------------------------------
-
-void primeFactorization(int n)
-{
-    int range=100001;
-    int primes[range];
-    int primeFactor[range];
-    for(int i=2;i<range;i++)
+    primes.push_back(2);
+    bool isPrime[105];
+    for(int i=3;i<101;i+=2)
     {
-        primes[i]=1;
-    }
-    for(int i=2;i<range;i++)
-    {
-        if(primes[i])
+        if(isPrime[i]==0)
         {
-            primeFactor[i]=i;
-            for(int j=2;i*j<range;j++)
+            // start erasing from i*i, because all numbers between
+            // i and i*i already erased
+            for(int j=i*i;j<101;j+=i*2)
             {
-                primes[i*j]=0;
-                if(primeFactor[i*j]==0)
-                {
-                    primeFactor[i*j]=i;
-                }
+                // i and j both are odd now
+                // so if we add i with j then j will be even (odd+odd=even)
+                // se we add i+i with j then j will be odd (odd + (odd+odd=even)) 
+                isPrime[j]=1;
             }
         }
     }
-    while(n!=1)
-    {
-        cout << primeFactor[n] << endl;
-        n/=primeFactor[n];
-    }
+    for(int i=3;i<101;i+=2)
+    if(isPrime[i]==0) 
+    primes.push_back(i);
 }
 
+
+int main()
+{
+    cout << "97 is a " << (isPrime(97) ? "Prime" : "Nonprime") << " number." << endl;
+
+    primeGenerate();
+    for(auto &prime : primes)
+    {
+        cout << prime << " ";
+    }
+    cout << endl;
+
+    primes.clear();
+
+    primeGenerateOptimized();
+    for(auto &prime : primes)
+    {
+        cout << prime << " ";
+    }
+    cout << endl;
+
+
+    return 0;
+}
